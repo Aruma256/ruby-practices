59ef4be0a2c598ae33287751a3047a16904dd4db
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-02-28 11:36:48 +0900

    rubocop対応

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
@@ -8,8 +9,8 @@ year = today.year
 month = today.month
 
 opt = OptionParser.new
-opt.on('-m', '--month N', Integer) {|val| month = val}
-opt.on('-y', '--year N', Integer) {|val| year = val}
+opt.on('-m', '--month N', Integer) { |val| month = val }
+opt.on('-y', '--year N', Integer) { |val| year = val }
 opt.parse!(ARGV)
 
 lines = []
@@ -19,19 +20,15 @@ lines << ['  '] * first_day.wday
 
 date = first_day
 while date.month == month
-  if lines.last.size == 7
-    lines << []
-  end
+  lines << [] if lines.last.size == 7
   date_str = date.day.to_s.rjust(2)
-  if date == today
-    date_str = "\e[;7m#{date_str}\e[m"
-  end
+  date_str = "\e[;7m#{date_str}\e[m" if date == today
   lines.last << date_str
   date = date.next_day
 end
 
 puts "      #{month}月 #{year}"
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 lines.each do |line|
-  puts line.join(" ")
+  puts line.join(' ')
 end
