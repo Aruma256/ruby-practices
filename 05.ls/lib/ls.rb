adab7b37d8324611e3de235cebb238c209851869
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-03-04 17:08:17 +0900

    lsコマンドを作る の解答を追加

---
+++ 05.ls/lib/ls.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'pathname'
+
+DEFAULT_COLUMN_COUNT = 3
+
+def main
+  target_path = ARGV[0] ? Pathname.new(ARGV[0]) : Pathname.getwd
+  filenames = target_path.glob('*').map { |item| item.basename.to_s }
+  table = to_table(filenames)
+  lines = convert_table_into_lines(table)
+  puts lines
+end
+
+def to_table(filenames, column_count: DEFAULT_COLUMN_COUNT)
+  filenames << nil until filenames.length.modulo(column_count).zero?
+  table = []
+  filenames.each_slice(column_count) do |slice|
+    table << slice
+  end
+  table
+end
+
+def convert_table_into_lines(table)
+  table.map! do |row|
+    max_length = row.compact.map(&:length).max
+    row.map do |element|
+      element&.ljust(max_length)
+    end
+  end
+  table.transpose.map do |row|
+    row.join(' ').strip
+  end
+end
+
+main if __FILE__ == $PROGRAM_NAME # テスト時にmainが実行されることを避ける
