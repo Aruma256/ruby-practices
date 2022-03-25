c4495e16fabbf5132393bec09298b01ec4d65e45
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-03-25 10:49:12 +0900

    オプション -l の処理を切り分けた

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -21,30 +21,37 @@ def main
   if params[:l]
     total_blocks = filenames.sum { |filename| File::Stat.new(filename).blocks }
     puts "total #{total_blocks}"
-    table = to_long_table(filenames)
+    print_as_list(filenames)
   else
-    table = to_table(filenames)
+    print_as_table(filenames)
   end
+end
+
+def print_as_list(filenames)
+  puts to_list(filenames)
+end
+
+def print_as_table(filenames)
+  table = to_table(filenames)
   lines = convert_table_into_lines(table)
   puts lines
 end
 
-def to_long_table(filenames)
-  table = filenames.map do |filename|
+def to_list(filenames)
+  filenames.map do |filename|
     stat = File.lstat(filename)
     [
       to_permission_string(stat),
-      stat.nlink,
-      Etc.getpwuid(stat.uid).name,
-      Etc.getgrgid(stat.gid).name,
-      stat.size,
-      stat.mtime.month,
-      stat.mtime.day,
+      stat.nlink.to_s.rjust(8),
+      Etc.getpwuid(stat.uid).name.rjust(8),
+      Etc.getgrgid(stat.gid).name.rjust(8),
+      stat.size.to_s.rjust(8),
+      stat.mtime.month.to_s.rjust(2),
+      stat.mtime.day.to_s.rjust(2),
       stat.mtime.strftime('%H:%M'),
-      stat.symlink? ? "#{filename} -> #{File.readlink(filename)}" : filename
-    ]
+      (stat.symlink? ? "#{filename} -> #{File.readlink(filename)}" : filename).ljust(64)
+    ].join(' ')
   end
-  table.transpose
 end
 
 def to_permission_string(stat)
@@ -68,13 +75,9 @@ end
 
 def convert_table_into_lines(table)
   table.map! do |row|
-    max_length = row.compact.map { |elem| elem.to_s.length }.max
+    max_length = row.compact.map(&:length).max
     row.map do |element|
-      if element.is_a?(Integer)
-        element.to_s.rjust(max_length)
-      else
-        element&.ljust(max_length)
-      end
+      element&.ljust(max_length)
     end
   end
   table.transpose.map do |row|
